70cdb238c0aaac3344a5112857fa35503ee5af68
fatimababiker <120206786+fatimababiker@users.noreply.github.com>
2022-12-17 21:26:57 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+include('connect.php');
+if(isset($_POST['Send'])){
+    
+    $YOURORDER =$_POST['ordername'];
+    $YOURORDERCOUNT =$_POST['ordercount'];
+    $YOURPHONE =$_POST['phoneorder'];
+ $insert ="INSERT INTO orders( ordername , ordercount , phoneorder) VALUES( '$YOURORDER','$YOURORDERCOUNT','$YOURPHONE')";
+ mysqli_query($con,$insert);  
+}
+if(isset($_POST['Send'])){
+ echo 'تم  قبول الطلب'; 
+}
+?>
